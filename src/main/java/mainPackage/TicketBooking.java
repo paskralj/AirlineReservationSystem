4e81fc435836bdf1f3c5a4ad437e97b57b4ddaa1
Tomo <suba213@hotmail.com>
2023-09-15 11:29:10 +0200

    Book button completed

--- src/main/java/mainPackage/TicketBooking.java
+++ src/main/java/mainPackage/TicketBooking.java
@@ -5,12 +5,15 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Vector;
 
 import javax.swing.DefaultComboBoxModel;
@@ -21,20 +24,32 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
 import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
 import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.table.DefaultTableModel;
 
 public class TicketBooking extends JInternalFrame {
 	private JTable table;
 	private JComboBox comboBox;
 	private JComboBox comboBox_1;
+	private JLabel lblNewLabel_14;
+	private JLabel lblNewLabel_14_1;
+	private JLabel lblNewLabel_14_2;
+	private JLabel lblNewLabel_14_4;
+	private JLabel lblNewLabel_4;
 
 	Connection con;
 	PreparedStatement ps;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
+	private JLabel lblNewLabel_18;
+	private JLabel lblNewLabel_16;
+	private JSpinner spinner;
+	private JComboBox comboBox_2;
 
 	/**
 	 * Launch the application.
@@ -105,6 +120,12 @@ public class TicketBooking extends JInternalFrame {
 		panel.add(btnNewButton);
 
 		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				fillTable();
+			}
+		});
 		scrollPane.setBounds(10, 175, 574, 181);
 		getContentPane().add(scrollPane);
 
@@ -125,7 +146,7 @@ public class TicketBooking extends JInternalFrame {
 		lblNewLabel_3.setBounds(31, 11, 109, 36);
 		panel_1.add(lblNewLabel_3);
 
-		JLabel lblNewLabel_4 = new JLabel("Ticket NO");
+		lblNewLabel_4 = new JLabel("");
 		lblNewLabel_4.setForeground(Color.BLUE);
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 17));
 		lblNewLabel_4.setBounds(41, 58, 99, 48);
@@ -178,6 +199,129 @@ public class TicketBooking extends JInternalFrame {
 		textField_2.setBounds(123, 91, 137, 20);
 		panel_2.add(textField_2);
 		textField_2.setColumns(10);
+
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(Color.ORANGE);
+		panel_3.setBounds(10, 374, 574, 319);
+		getContentPane().add(panel_3);
+		panel_3.setLayout(null);
+
+		JLabel lblNewLabel_8 = new JLabel("Flight number:");
+		lblNewLabel_8.setForeground(Color.MAGENTA);
+		lblNewLabel_8.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_8.setBounds(10, 11, 112, 31);
+		panel_3.add(lblNewLabel_8);
+
+		JLabel lblNewLabel_9 = new JLabel("Flight name:");
+		lblNewLabel_9.setForeground(Color.MAGENTA);
+		lblNewLabel_9.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_9.setBounds(10, 53, 112, 35);
+		panel_3.add(lblNewLabel_9);
+
+		JLabel lblNewLabel_10 = new JLabel("Depart time");
+		lblNewLabel_10.setForeground(Color.MAGENTA);
+		lblNewLabel_10.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_10.setBounds(10, 99, 112, 36);
+		panel_3.add(lblNewLabel_10);
+
+		JLabel lblNewLabel_11 = new JLabel("Class:");
+		lblNewLabel_11.setForeground(Color.MAGENTA);
+		lblNewLabel_11.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_11.setBounds(10, 146, 112, 30);
+		panel_3.add(lblNewLabel_11);
+
+		JLabel lblNewLabel_12 = new JLabel("Price:");
+		lblNewLabel_12.setForeground(Color.MAGENTA);
+		lblNewLabel_12.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_12.setBounds(10, 187, 112, 31);
+		panel_3.add(lblNewLabel_12);
+
+		JLabel lblNewLabel_13 = new JLabel("Seats:");
+		lblNewLabel_13.setForeground(Color.MAGENTA);
+		lblNewLabel_13.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_13.setBounds(10, 229, 112, 31);
+		panel_3.add(lblNewLabel_13);
+
+		lblNewLabel_14 = new JLabel("");
+		lblNewLabel_14.setForeground(Color.BLUE);
+		lblNewLabel_14.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_14.setBounds(146, 11, 174, 31);
+		panel_3.add(lblNewLabel_14);
+
+		lblNewLabel_14_1 = new JLabel("");
+		lblNewLabel_14_1.setForeground(Color.BLUE);
+		lblNewLabel_14_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_14_1.setBounds(146, 53, 174, 31);
+		panel_3.add(lblNewLabel_14_1);
+
+		lblNewLabel_14_2 = new JLabel("");
+		lblNewLabel_14_2.setForeground(Color.BLUE);
+		lblNewLabel_14_2.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_14_2.setBounds(146, 99, 174, 31);
+		panel_3.add(lblNewLabel_14_2);
+
+		lblNewLabel_14_4 = new JLabel("");
+		lblNewLabel_14_4.setForeground(Color.BLUE);
+		lblNewLabel_14_4.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_14_4.setBounds(146, 187, 174, 31);
+		panel_3.add(lblNewLabel_14_4);
+
+		comboBox_2 = new JComboBox();
+		comboBox_2.setModel(new DefaultComboBoxModel(new String[] { "Economy", "Business", "First" }));
+		comboBox_2.setBounds(146, 152, 112, 22);
+		panel_3.add(comboBox_2);
+
+		spinner = new JSpinner();
+		spinner.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				calculation();
+			}
+		});
+		spinner.setBounds(146, 236, 59, 24);
+		panel_3.add(spinner);
+
+		JLabel lblNewLabel_15 = new JLabel("Date:");
+		lblNewLabel_15.setForeground(Color.MAGENTA);
+		lblNewLabel_15.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_15.setBounds(309, 11, 78, 31);
+		panel_3.add(lblNewLabel_15);
+
+		lblNewLabel_16 = new JLabel("");
+		lblNewLabel_16.setForeground(Color.BLUE);
+		lblNewLabel_16.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_16.setBounds(381, 11, 92, 31);
+		panel_3.add(lblNewLabel_16);
+
+		JButton btnNewButton_2 = new JButton("Book");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				generateFlightId();
+				bookButton();
+			}
+		});
+		btnNewButton_2.setBounds(298, 229, 104, 41);
+		panel_3.add(btnNewButton_2);
+
+		JButton btnNewButton_2_1 = new JButton("Cancel");
+		btnNewButton_2_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		btnNewButton_2_1.setBounds(414, 229, 104, 41);
+		panel_3.add(btnNewButton_2_1);
+
+		JLabel lblNewLabel_17 = new JLabel("Overall price:");
+		lblNewLabel_17.setForeground(Color.MAGENTA);
+		lblNewLabel_17.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_17.setBounds(309, 91, 112, 39);
+		panel_3.add(lblNewLabel_17);
+
+		lblNewLabel_18 = new JLabel("0");
+		lblNewLabel_18.setForeground(Color.BLUE);
+		lblNewLabel_18.setFont(new Font("Tahoma", Font.BOLD, 21));
+		lblNewLabel_18.setBounds(362, 140, 134, 47);
+		panel_3.add(lblNewLabel_18);
 
 	}
 
@@ -240,7 +384,86 @@ public class TicketBooking extends JInternalFrame {
 				String lastName = rs.getString("lastname");
 				textField_1.setText(firstName);
 				textField_2.setText(lastName);
-			}	
+			}
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void fillTable() {
+		DefaultTableModel df = (DefaultTableModel) table.getModel();
+		int selectedRow = table.getSelectedRow();
+		lblNewLabel_14.setText(df.getValueAt(selectedRow, 0).toString());
+		lblNewLabel_14_1.setText(df.getValueAt(selectedRow, 1).toString());
+		lblNewLabel_14_2.setText(df.getValueAt(selectedRow, 6).toString());
+		lblNewLabel_14_4.setText(df.getValueAt(selectedRow, 7).toString());
+		lblNewLabel_16.setText(df.getValueAt(selectedRow, 4).toString());
+	}
+
+	public void generateFlightId() {
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost/airline", "root", "");
+			Statement s = con.createStatement();
+			ResultSet rs = s.executeQuery("Select MAX(id) from ticket");
+			rs.next();
+			rs.getString("MAX(id)");
+			if (rs.getString("MAX(id)") == null) {
+				lblNewLabel_4.setText("TID001");
+			} else {
+				long id = Long.parseLong(rs.getString("MAX(id)").substring(3, rs.getString("MAX(id)").length()));
+				id++;
+				lblNewLabel_4.setText("TID" + String.format("%03d", id));
+			}
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void calculation() {
+		int price = Integer.parseInt(lblNewLabel_14_4.getText());
+		int quantity = Integer.parseInt(spinner.getValue().toString());
+		int total = price * quantity;
+
+		lblNewLabel_18.setText(String.valueOf(total));
+	}
+
+	public void bookButton() {
+		String ticketId = lblNewLabel_4.getText();
+		String flightId = lblNewLabel_14.getText();
+		String customerId = textField.getText();
+		String avioClass = comboBox_2.getSelectedItem().toString().trim();
+		String price = lblNewLabel_18.getText();
+		String seats = spinner.getValue().toString();
+		String date = lblNewLabel_16.getText();
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost/airline", "root", "");
+			ps = con.prepareStatement(
+					"insert into ticket (id,flightid,custid,class,price,seats,date)values(?,?,?,?,?,?,?)");
+			ps.setString(1, ticketId);
+			ps.setString(2, flightId);
+			ps.setString(3, customerId);
+			ps.setString(4, avioClass);
+			ps.setString(5, price);
+			ps.setString(6, seats);
+			ps.setString(7, date);
+			ps.executeUpdate();
+
+			JOptionPane.showMessageDialog(null, "Ticked booked ! ");
 
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
